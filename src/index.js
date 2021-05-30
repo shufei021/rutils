407f80fd64dc90f6c2c44271fc829dee77b65d9a
shufei021 <1017981699@qq.com>
2021-05-30 22:33:37 +0800

    feat: 尝试打包集成

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import isEmpty from './lib/isEmpty'
+import isEquals from './lib/isEquals'
+import isArray from './lib/isArray'
+import isBoolean from './lib/isBoolean'
+import isDate from './lib/isDate'
+import isFunction from './lib/isFunction'
+import isNull from './lib/isNull'
+import isNumber from './lib/isNumber'
+import isObject from './lib/isObject'
+import isRegExp from './lib/isRegExp'
+import isString from './lib/isString'
+import isType from './lib/isType'
+import isUndefined from './lib/isUndefined'
+import union from './lib/union'
+import intersection from './lib/intersection'
+export default {
+    isEmpty,
+    isEquals,
+    isArray,
+    isBoolean,
+    isDate,
+    isFunction,
+    isNull,
+    isNumber,
+    isObject,
+    isRegExp,
+    isString,
+    isType,
+    isUndefined,
+    union,
+    intersection
+}
