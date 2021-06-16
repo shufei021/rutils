da33fceca553e2ff9700a703f69e44033017e736
舒飞 <1017981699@qq.com>
2021-06-16 10:29:12 +0800

    feat: init

--- deloy.sh
+++ deloy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:rutils-docs
 
 cd -
 
