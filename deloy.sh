2cc44ebccd2fc2195eac8ae2e1015c44480c3090
舒飞 <1017981699@qq.com>
2021-06-16 10:17:09 +0800

    feat: init

--- deloy.sh
+++ deloy.sh
@@ -9,9 +9,19 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:shufei021/rutils.git master:rutils-docs
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
 cd -
+
+
