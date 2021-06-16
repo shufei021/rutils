0d54d3129d6141245925c16737c1e54452f24c54
舒飞 <1017981699@qq.com>
2021-06-16 11:10:29 +0800

    feat: init

--- deloy.sh
+++ deloy.sh
@@ -10,17 +10,15 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:rutils-docs
 
 cd -
 
