ad3f775951f4dcb482b0a01d823a35ddfaabd2f3
舒飞 <1017981699@qq.com>
2021-06-16 16:04:47 +0800

    feat: init

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,12 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:shufei021/rutils.git master:gh-pages
 
 cd -
 
