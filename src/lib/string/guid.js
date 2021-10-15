c13006ea03c362403d30832f7303568242487a5c
Free-Fly <1017981699@qq.com>
2021-10-15 15:30:09 +0800

    generate guid
    a another realization  about guid

--- src/lib/string/guid.js
+++ src/lib/string/guid.js
@@ -9,3 +9,12 @@ const guid = function () {
     return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4()
 }
 export default guid
+
+/**
+ Another realization  
+const guid = () =>
+            [2, 1, 1, 1, 3].reduce(
+                (p, c, i, a) => (p += Array.from({ length: c }, (_, i) => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)).join('') + (i !== a.length - 1 ? '-' : '')),
+                ''
+            )
+*/
