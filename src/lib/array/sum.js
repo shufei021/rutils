2e1f7024c29bd587e7fce2725fd3516377655cbd
rookie_fly <1017981699@qq.com>
2022-01-09 03:57:12 +0000

    Update sum.js

--- src/lib/array/sum.js
+++ src/lib/array/sum.js
@@ -4,7 +4,10 @@
  * @return {Number} 和
  */
 const sum = function (a, k) {
-    return a.reduce((p, c) => p + (k ? c[k] || 0 : c), 0)
+    let isFun = typeof k === 'function'
+    return a.reduce((p, c) =>{
+        return p + (isFun?k(c):  (k ? c[k] || 0 : c))
+    }, 0)
 }
 export default sum
 
@@ -29,4 +32,18 @@ sum(a1, 'age') //64
  * 
  * 
  * 
+ 
+const sum = function (a, k) {
+    let isFun = typeof k === 'function'
+    return a.reduce((p, c) =>{
+        return p + (isFun?k(c):  (k ? c[k] || 0 : c))
+    }, 0)
+}
+let a1 = [
+    { id: 1, name: '张三', age: 20,num:11,n:1 },
+    { id: 2, name: '李四', age: 21,num:22,n:2 },
+    { id: 3, name: '小二', age: 23,num:33,n:3}
+]
+console.log(sum(a1, 'num'))
+console.log(sum(a1, (item)=>item.age+item.num-item.n))
  */
