40380a12d4b697846f09df84c27f8d69bd7dd0c4
shufei <1017981699@qq.com>
2022-04-11 11:15:35 +0800

    feat: update

--- src/lib/dom/getMaxZIndex.js
+++ src/lib/dom/getMaxZIndex.js
@@ -2,6 +2,6 @@
  * @description: 获取html页面dom中最大的层级数
  * @return Number
  */
-export default getMaxZIndex = function () {
+export default function getMaxZIndex() {
     return Math.max(...[...document.querySelectorAll('*')].map(i => parseInt(getComputedStyle(i).zIndex) || 1))
 }
